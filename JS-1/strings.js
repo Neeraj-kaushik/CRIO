b3ded8e9e61f0b0642ed7de1cffb311067f0a65e
Neeraj <nkthecoder@gmail.com>
2023-09-30 17:30:27 +0530

    JS-1

---
+++ JS-1/strings.js
@@ -0,0 +1,10 @@
+// strings : combination of characters enclosed in single or double quotes
+// they are immutable. so we can access the character by square notation or charAt() function.
+// to find the length of string we can use the .length property.
+let a="Neeraj"
+console.log(a.length)
+console.log(a[0])
+console.log(a.charAt(4))
+for(let i=0; i<=a.length;i++){
+    console.log(a[i])
+}
