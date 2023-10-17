097adaa541eb61c45600d2e2f54a639382985116
Neeraj <nkthecoder@gmail.com>
2023-10-17 17:20:43 +0530

    JS-1

---
+++ JS-1/functions.js
@@ -0,0 +1,41 @@
+//Here i will try some questions and how to use functions in js
+// Function: a function is a block of code which we create to perform specific task and we can all that as much time we want to call it in our program. it will save our time to write same code again and again.
+// in js we can define a funtion using this syntax:
+// function(keyword) functionName(parameters){
+//     code
+// }
+// Lets try some questions now.
+
+//ques-1: program to check if two number are equal or not.
+// function checkNum(c,d){
+//     if(c===d){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+
+// let a=3;
+// let b=5;
+// if(checkNum(a,b)){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+//Question2: grade students based on marks
+// function grade(marks){
+//     if(marks>90){
+//         return 'A'
+//     }else if(marks>80 && marks<=90){
+//         return 'B'
+//     }else if(marks>70 && marks<=80){
+//         return 'C'
+//     }else{
+//         return 'Fail'
+//     }
+// }
+
+// let marks=70;
+// console.log(grade(marks))
+
