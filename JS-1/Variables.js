a9806ed6fc0242f2da42b402e4e11a4c7163c8e8
Neeraj <nkthecoder@gmail.com>
2023-10-17 16:53:56 +0530

    JS-1

---
+++ JS-1/Variables.js
@@ -0,0 +1,6 @@
+//Variables:- variables are container which stores some value/data. In it we decalre the variable name and initialize a value to the varibale.
+//Example
+//let a ; this is declaration without initialization
+// let b=5-; this is declaration as well as initialization.
+let a=25;
+console.log(a);
