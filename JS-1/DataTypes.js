f0841419f80683d8809cc8aa2c23dc03acff45ad
Neeraj <nkthecoder@gmail.com>
2023-10-17 16:48:23 +0530

    JS-1

---
+++ JS-1/DataTypes.js
@@ -0,0 +1,20 @@
+// In JS we have mainly two types of data types one is primitive data type and second is non primitive data types
+// Primitive data types: these are also known as fundamental data types because they directly stores the data. In js there are Number, boolean, string, null, undefined these data types under primitive one
+// Non primitive data types: these are little more complex rather than storing the value they store the reference to the value . these are made by combining primitive data types eg are Array, Object. 
+// Numbers :- These are the number in js eg 1,2,3,4,22.5 etc 
+// boolean :- it stores true or false as a value 
+// string :- combination of character inside double quotes. eg "12","neeraj"
+// null:- this is the value we generally assign to any variable if we do not want any initial value to it so it will strat pointing to null. this is something done intentially. eg let a = null
+// undefined:- when we forget to assign initial value to any of the variable it will auto point to undefined. eg let a; typeof(a). it will give us output as undefined
+// there examples are below
+
+let a=1;
+let b="neeraj";
+let c=true;
+let d=null;
+let e;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
