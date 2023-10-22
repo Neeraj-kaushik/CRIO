d0ba44861d89c7eeb7378cc64df4b49e1f14a6b9
Neeraj <nkthecoder@gmail.com>
2023-10-22 09:33:12 +0530

    FE-2

--- FE-2/Session7/browserEvents/index.js
+++ FE-2/Session7/browserEvents/index.js
@@ -3,11 +3,25 @@ const btn=document.getElementById("btn1");
 const btn3=document.getElementById("btn2");
 let iny=para.textContent
 
-btn.addEventListener('click',()=>{
-    iny=Number(iny)+1
-    para.textContent=iny
-})
-btn3.addEventListener('click',()=>{
-    iny=Number(iny)-1
-    para.textContent=iny
+// btn.addEventListener('click',()=>{
+//     iny=Number(iny)+1
+//     para.textContent=iny
+// })
+// btn3.addEventListener('click',()=>{
+//     iny=Number(iny)-1
+//     para.textContent=iny
+// })
+
+//using one function as event handler using event object
+function counter(val){
+    if(val==='btn1'){
+        iny=Number(iny)+1
+        para.textContent=iny
+    }else if(val==='btn2'){
+        iny=Number(iny)-1
+        para.textContent=iny
+    }
+}
+document.addEventListener('click',(event)=>{
+    counter(event.target.id)
 })
